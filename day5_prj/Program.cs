de433674e297bbb9330c3d06a3112dfb8ab529f5
Ashu Gupta <ashgupta25@publicisgroupe.net>
2020-09-11 15:57:51 +0530

    humanizer

--- day5_prj/Program.cs
+++ day5_prj/Program.cs
@@ -1,17 +1,36 @@
 ﻿using System;
+using Humanizer;
 
 namespace day5_prj
 {
     class Program
     {
+
+        int a,b,c;
         static void Main(string[] args)
-        {
-            System.Console.WriteLine("Enter two numbers");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int c = a + b;
-            // Formatted output
-            System.Console.WriteLine("Sum of {0} and {1} is {2}",a,b,c);
+        {   
+
+            /*
+              convert number into words  
+            */
+            // var ob = new Program();
+            // System.Console.WriteLine("Enter two numbers");
+            // ob.a = Convert.ToInt32(Console.ReadLine());
+            // ob.b = Convert.ToInt32(Console.ReadLine());
+            // ob.c = ob.a + ob.b;
+            // // Formatted output
+            // System.Console.WriteLine("Sum of {0} and {1} is {2}",ob.a,ob.b,ob.c);
+
+            Console.Write("Enter amount:");
+            int amt = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine(amt.ToWords());
+            System.Console.WriteLine("Car".Pluralize());
+
+
         }
+
+
+
     }
 }
