66ec0d0e8552a2301cfd2307badc1fbaf17d259a
Ashu Gupta <ashgupta25@publicisgroupe.net>
2020-09-11 15:44:07 +0530

    Initial Commit

---
+++ day5_lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace day5_lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ day5_prj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace day5_prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter two numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            // Formatted output
+            System.Console.WriteLine("Sum of {0} and {1} is {2}",a,b,c);
+        }
+    }
+}
