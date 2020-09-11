93d4bca9e1cd6f88f848f9739b990e25353639f9
Ashu Gupta <ashgupta25@publicisgroupe.net>
2020-09-11 16:01:04 +0530

    tables

--- day5_prj/Program.cs
+++ day5_prj/Program.cs
@@ -21,11 +21,24 @@ namespace day5_prj
             // // Formatted output
             // System.Console.WriteLine("Sum of {0} and {1} is {2}",ob.a,ob.b,ob.c);
 
-            Console.Write("Enter amount:");
-            int amt = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("Enter amount:");
+            // int amt = Convert.ToInt32(Console.ReadLine());
 
-            System.Console.WriteLine(amt.ToWords());
-            System.Console.WriteLine("Car".Pluralize());
+            // System.Console.WriteLine(amt.ToWords());
+            // System.Console.WriteLine("Car".Pluralize());
+
+            // Accept a number a print mathematical table
+
+            Console.Write("Enter number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                 System.Console.WriteLine("{0}x{1}={2}",n,i,(n*i));
+            }
+
+
+
 
 
         }
