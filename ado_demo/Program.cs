cd2e227f621161e02dc1e94fcbcd24d707c8b48e
Ashu Gupta <ashgupta25@publicisgroupe.net>
2020-09-18 15:25:13 +0530

    storeproc

--- ado_demo/Program.cs
+++ ado_demo/Program.cs
@@ -11,11 +11,20 @@ namespace ado_demo
         static void Main(string[] args)
         {
             StudentDAO studentDAO = new StudentDAO();
-            List<StudentDTO> students = studentDAO.GetAllStudents();
-            foreach (var item in students)
-            {
-                System.Console.WriteLine(item.fname + " " + item.sid + " " + item.dob);
-            }
+            // List<StudentDTO> students = studentDAO.GetStudentByID("1005");
+            // foreach (var item in students)
+            // {
+            //     System.Console.WriteLine(item.fname + " " + item.sid + " " + item.dob);
+            // }
+
+            System.Console.WriteLine("Enter student details: sid fname dob");
+            StudentDTO student = new StudentDTO();
+            student.sid = Console.ReadLine();
+            student.fname = Console.ReadLine();
+            student.dob = Console.ReadLine();
+
+            // System.Console.WriteLine(studentDAO.InsertStudent(student));
+            System.Console.WriteLine(studentDAO.InsertStudentUsingProcedure(student));
         }
     }
 
--- ado_demo/data_access.cs
+++ ado_demo/data_access.cs
@@ -1,11 +1,11 @@
 using System.Collections.Generic;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace ado_demo
 {
     class StudentDAO
     {
-
         public List<StudentDTO> GetAllStudents(){
             MySqlDataReader reader = null;
             List<StudentDTO> students = null;
@@ -19,8 +19,8 @@ namespace ado_demo
                 while (reader.Read())
                 {
                     StudentDTO ob = new StudentDTO();
-                    ob.sid = (string)reader[0];
-                    ob.fname =(string) reader[1];
+                    ob.sid = (string) reader[0];
+                    ob.fname = (string) reader[1];
                     ob.dob = reader[2].ToString();
                     students.Add(ob);
                 }
@@ -34,6 +34,110 @@ namespace ado_demo
             }
             return students;
         }
+
+        public List<StudentDTO> GetStudentByID(string sid){
+            MySqlDataReader reader = null;
+            List<StudentDTO> students = null;
+            try
+            {
+                MySqlConnection co = MConnect.getConnection();
+                co.Open();
+                string query = "select * from stud where sid=@a1";
+
+                MySqlCommand com = new MySqlCommand(query,co);
+                com.Parameters.AddWithValue("@a1",sid);
+                reader = com.ExecuteReader();
+                students = new List<StudentDTO>();
+                while (reader.Read())
+                {
+                    StudentDTO ob = new StudentDTO();
+                    ob.sid = (string) reader[0];
+                    ob.fname = (string) reader[1];
+                    ob.dob = reader[2].ToString();
+                    students.Add(ob);
+                }
+            }
+            catch (System.Exception e)
+            {
+                throw new System.Exception(e.Message);
+            }
+            finally{
+                reader.Close();
+            }
+            return students;
+        }
+
+
+        public string InsertStudent(StudentDTO student){
+            try
+            {
+                MySqlConnection co = MConnect.getConnection();
+                co.Open();
+                string query = "insert into stud values(@a1,@a2,@a3)";
+
+                MySqlCommand com = new MySqlCommand(query,co);
+                com.Parameters.AddWithValue("@a1",student.sid);
+                com.Parameters.AddWithValue("@a2",student.fname);
+                com.Parameters.AddWithValue("@a3",student.dob);
+                com.ExecuteNonQuery();          
+            }
+            catch (System.Exception e)
+            {
+                throw new System.Exception(e.Message);
+            }
+            finally{
+                
+            }
+            return "inserted!";
+        }
+
+
+
+
+        public string InsertStudentUsingProcedure(StudentDTO student){
+            try
+            {
+                MySqlConnection co = MConnect.getConnection();
+                co.Open();
+                string query = "p2";
+
+                MySqlCommand com = new MySqlCommand(query,co);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("a1",student.sid);
+                com.Parameters.AddWithValue("a2",student.fname);
+                com.Parameters.AddWithValue("a3",student.dob);
+                com.ExecuteNonQuery();          
+            }
+            catch (System.Exception e)
+            {
+                throw new System.Exception(e.Message);
+            }
+            finally{
+                
+            }
+            return "inserted!";
+        }
+
+        //delete
+
+        public StudentDTO deleteStudentByID(string sid){
+            
+        }
+
+
+
+
+
+
+
+
+
+
+        //update
+
+
+        
+        
         
 
 
